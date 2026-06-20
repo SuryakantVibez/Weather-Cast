916fa673df8d76c9da1f0f3c8c5df5bdb801557c
SuryakantVibez <suryakant.yadav2007@gmail.com>
2026-06-20 13:22:00 +0530

    Static UI complete

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 "https://api.weatherapi.com/v1/current.json?key=964aefc45f084abf97c30437262105&q=" +
   cityName +
   "&aqi=yes";
+
+console.log(document.querySelector(".searchSubmitBtn"));
+document.querySelector(".searchSubmitBtn").addEventListener("click", () => {
+  document.documentElement.requestFullscreen();
+});
