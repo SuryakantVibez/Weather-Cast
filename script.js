645c0f20fd3b41d345856c1e8475592a4aaa9a3b
SuryakantVibez <suryakant.yadav2007@gmail.com>
2026-06-22 13:02:19 +0530

    Enabled searching for locations and fetching data

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-"https://api.weatherapi.com/v1/current.json?key=964aefc45f084abf97c30437262105&q=" +
-  cityName +
-  "&aqi=yes";
+//Temp Fullscreen function
 
-console.log(document.querySelector(".searchSubmitBtn"));
-document.querySelector(".searchSubmitBtn").addEventListener("click", () => {
-  document.documentElement.requestFullscreen();
-});
+// document.querySelector(".searchSubmitBtn").addEventListener("click", () => {
+//   document.documentElement.requestFullscreen();
+// });
+
+// Searching location
+
+const searchBar = document.querySelector("#locationInput");
+
+document.querySelector(".searchSubmitBtn").onclick = () => {
+  const locationName = searchBar.value;
+  searchBar.value = null;
+  console.log(locationName);
+  getWeather(locationName);
+};
+
+const api = "964aefc45f084abf97c30437262105";
+
+// Fetching from API
+
+async function getWeather(locationName) {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=" +
+      api +
+      "&q=" +
+      locationName +
+      "&aqi=yes",
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+  console.log(" ");
+  console.log(response);
+}
