1b07f875855b01edba8d314fd1fff262aeb0aaad
SuryakantVibez <suryakant.yadav2007@gmail.com>
2026-06-25 14:47:58 +0530

    Added humidity, replacement for AQI

--- script.js
+++ script.js
@@ -17,20 +17,51 @@ document.querySelector(".searchSubmitBtn").onclick = () => {
 
 const api = "964aefc45f084abf97c30437262105";
 
-// Fetching from API
+// Fetch and Update data
 
+// Fetch
 async function getWeather(locationName) {
   const response = await fetch(
     "https://api.weatherapi.com/v1/current.json?key=" +
       api +
       "&q=" +
       locationName +
-      "&aqi=yes",
+      "&aqi=no",
   );
 
   const data = await response.json();
-
   console.log(data);
-  console.log(" ");
-  console.log(response);
+
+  // Update
+
+  document.querySelector(".tempText").textContent =
+    Math.round(data.current.temp_c) + "°C";
+  document.querySelector(".condition").textContent =
+    data.current.condition.text;
+  document.querySelector(".cityLocation").textContent =
+    data.location.name + ", " + data.location.region;
+  document.querySelector(".windDirection").textContent = data.current.wind_dir;
+  document.querySelector(".windDegree").textContent = data.current.wind_degree;
+
+  //Time wizardary
+
+  document.querySelector("#localTime").textContent = new Date(
+    data.location.localtime.replace(" ", "T"),
+  ).toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  // Humidity
+
+  document.querySelector(".humidity").textContent = data.current.humidity + "%";
+  if (data.current.humidity <= 33) {
+    locationHumidity = "Low";
+  } else if (data.current.humidity >= 34 && data.current.humidity <= 66) {
+    locationHumidity = "Medium";
+  } else if (data.current.humidity > 66) {
+    locationHumidity = "High";
+  }
+  document.querySelector(".humidityState").textContent = locationHumidity;
 }
